c62af0a9d3e54d44f4b09a5db41a4807d9bcc64d
Rachel <rjcrter11@gmail.com>
2020-11-14 15:02:33 -0600

    builds useProjects hook for use with firebase

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,6 +5,7 @@ import moment from 'moment'
 
 export const useTasks = selectedProject => {
     const [tasks, setTasks] = useState([]);
+    const [archivedTasks, setArchivedTasks] = useState([])
 
     useEffect(() => {
         let unsubscribe = firebase
@@ -20,5 +21,47 @@ export const useTasks = selectedProject => {
                     : selectedProject === 'INBOX' || selectedProject === 0
                         ? (unsubscribe = unsubscribe.where('date', '==', ''))
                         : unsubscribe;
+
+        unsubscribe = unsubscribe.onSnapshot(snapshot => {
+            const newTasks = snapshot.docs.map(task => ({
+                id: task.id,
+                ...task.data(),
+            }));
+            setTasks(
+                selectedProject === 'NEXT_7'
+                    ? newTasks.filter(
+                        task => moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+                            task.archrived !== true
+                    )
+                    : newTasks.filter(task => task.archrived !== true)
+            );
+            setArchivedTasks(newTasks.filter(task => task.archrived !== false));
+        });
+        return () => unsubscribe();
     }, [selectedProject])
+    return { tasks, archivedTasks };
+};
+
+export const useProjects = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        firebase
+            .firestore()
+            .collection('projects')
+            .where('userId', '==', 'ajdksnvcl134l')
+            .orderBy('projectId')
+            .get()
+            .then(snapshot => {
+                const allProjects = snapshot.docs.map(project => ({
+                    ...project.data(),
+                    docId: project.id,
+                }));
+                if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+                    setProjects(allProjects);
+                }
+            });
+    }, [projects]);
+
+    return { projects, setProjects };
 }
