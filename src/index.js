14d1218614584ba1177e25ba03c1ba3a0e7a76df
Rachel <rjcrter11@gmail.com>
2020-11-16 16:49:25 -0600

    adds favicon, logo, styles header

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
-
+import './App.scss'
 
 render(
   <React.StrictMode>
